4bac5b47dee5140baea103cd4e304047ad5dceca
Carson Lam <carsonlam@Carsons-MBP-2.lan>
2023-12-23 08:10:04 -0800

    readme

---
+++ part1setup/hello_world/main.rs
@@ -0,0 +1,11 @@
+// this code goes in source file  main.rs
+/*
+To compile this file:
+   hello_world $ rustc main.rs
+To run this file from your terminal while withing the same director
+   hello_world $ ./main
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
